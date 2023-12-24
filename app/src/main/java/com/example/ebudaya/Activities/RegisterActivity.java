1660b7d05f04c5fb40e017375ebbbdc523384b51
AddinSuhaimi <addinsuhaimi@gmail.com>
2023-12-24 21:12:55 +0800

    added packages for organisation renamed the first files for register activity

--- app/src/main/java/com/example/ebudaya/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/ebudaya/Activities/RegisterActivity.java
@@ -1,15 +1,17 @@
-package com.example.ebudaya;
+package com.example.ebudaya.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ebudaya.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
     }
 }
