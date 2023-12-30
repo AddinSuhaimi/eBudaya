53afe8ffd3a734875bf2c9a1da807326d3df9975
yumdmb <elvissawing.muran@gmail.com>
2023-12-30 16:53:42 +0800

    add and update nav header on for main page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,6 +48,10 @@ dependencies {
 
     // Add the dependency for the Firebase Storage Library
     implementation("com.google.firebase:firebase-storage")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
 
 
     // Add the dependencies for any other desired Firebase products
