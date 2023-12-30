1d9a83a630eede97b2bae585e3de28cdcb233af8
AddinSuhaimi <addinsuhaimi@gmail.com>
2023-12-31 02:11:53 +0800

    added profile pic during sign up

--- app/src/main/java/com/example/ebudaya/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/ebudaya/Activities/RegisterActivity.java
@@ -1,26 +1,42 @@
 package com.example.ebudaya.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.ebudaya.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView ImgUserPhoto;
+    static int PReqCode = 1;
+    static int REQUESTCODE = 1;
+    Uri pickedImgUri;
     private EditText userEmail,userPassword,userPassword2,userName;
     private ProgressBar loadingProgress;
     private Button regBtn;
@@ -53,22 +69,85 @@ public class RegisterActivity extends AppCompatActivity {
                 final String password2 = userPassword2.getText().toString();
                 final String name = userName.getText().toString();
 
-                if(email.isEmpty() || name.isEmpty() || password.isEmpty() || !password.equals(password2)) {
+                if (email.isEmpty() || name.isEmpty() || password.isEmpty() || !password.equals(password2)) {
                     //something goes wrong : all fields must be filled
                     //we need to display an error message
                     showMessage("Please verify all fields");
                     regBtn.setVisibility(View.VISIBLE);
                     loadingProgress.setVisibility(View.INVISIBLE);
-                }
-                else {
+                } else {
                     //everything is ok and all fields are filled now we can start creating user account
                     // createUserAccount method will try to create the user if the email is valid
 
-                    createUserAccount(email,name,password);
+                    createUserAccount(email, name, password);
                 }
 
             }
         });
+
+        //inu views
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= 22) {
+
+                    //checkAndRequestForPermission();
+                    openGallery();
+
+                }
+                else {
+                    openGallery();
+                }
+            }
+        });
+
+    }
+
+    private void openGallery() {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESTCODE);
+
+    }
+
+    private void checkAndRequestForPermission() {
+
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                Toast.makeText(RegisterActivity.this,"Please accept required permissions",Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                                    PReqCode);
+            }
+
+        }
+        else
+            openGallery();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUESTCODE && data != null) {
+
+            //the user has successfully picked an image
+            //we need to save its reference to a Uri variable
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
+
+        }
+
     }
 
     private void createUserAccount(String email, String name, String password) {
@@ -100,32 +179,33 @@ public class RegisterActivity extends AppCompatActivity {
     //update user photo and name
     private void updateUserInfo(String name, FirebaseUser currentUser) {
 
-        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
-                .setDisplayName(name)
-                .build();
+            UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(name)
+                    .build();
 
-        currentUser.updateProfile(profileUpdate)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            //user info updated successfully
-                            showMessage(("Register Complete"));
-                            updateUI();
+            currentUser.updateProfile(profileUpdate)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                //user info updated successfully
+                                showMessage(("Register Complete"));
+                                updateUI();
+                            }
                         }
-                    }
-                });
+                    });
+
     }
 
     private void updateUI() {
 
-        Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
-        startActivity(homeActivity);
+        Intent HomeActivity = new Intent(getApplicationContext(), com.example.ebudaya.Activities.HomeActivity.class);
+        startActivity(HomeActivity);
         finish();
 
     }
 
-    //simple method to show toast message0
+    //simple method to show toast message
     private void showMessage(String message) {
 
         Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
