2340c8d78d868046652f00f0e35816de53c514b0
yumdmb <elvissawing.muran@gmail.com>
2024-01-05 10:30:28 +0800

    use Home instead of HomeActivity

--- app/src/main/java/com/example/ebudaya/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/ebudaya/Activities/RegisterActivity.java
@@ -224,7 +224,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void updateUI() {
 
-        Intent HomeActivity = new Intent(getApplicationContext(), com.example.ebudaya.Activities.HomeActivity.class);
+        Intent HomeActivity = new Intent(getApplicationContext(), com.example.ebudaya.Activities.Home.class);
         startActivity(HomeActivity);
         finish();
 
