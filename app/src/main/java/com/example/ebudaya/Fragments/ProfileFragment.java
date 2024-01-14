2316221ab03ae260be217581f52f2c47c8dc87ce
AddinSuhaimi <addinsuhaimi@gmail.com>
2024-01-14 12:00:26 +0800

    added edit profile

--- app/src/main/java/com/example/ebudaya/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/ebudaya/Fragments/ProfileFragment.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ebudaya.R;
 
@@ -30,6 +33,18 @@ public class ProfileFragment extends Fragment {
 
     public ProfileFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button BtnEdit = view.findViewById(R.id.ProfilePageButtonEdit);
+        View.OnClickListener OCLEdit = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.nav_edit_profile);
+            }
+        };
+        BtnEdit.setOnClickListener(OCLEdit);
     }
 
     /**
